b222fce5ac4906830ee03dfa49469f6464475e53
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-29 13:03:10 +0300

    Add files via upload

--- Week5Activity2.py
+++ Week5Activity2.py
@@ -1,7 +1,23 @@
-#A program which demonstrates polymorphism
+#A program which demonstrates polymorphism in Vehicles with same action
 
 class Car:
     """The car moves by driving"""
     def move(self):
         #Prints the message
         print("Driving")
+        
+class Boat:
+    """Represents a boat which moves by sailing"""
+    def move(self):
+        print("Sailing")
+        
+class Plane:
+    """A plane moves by flying"""
+    def move(self):
+        print("Flying")
+        
+#A function that takes a vehicle object and calls its move() method
+def make_it_move(vehicle):
+    vehicle.move()
+        
+        
