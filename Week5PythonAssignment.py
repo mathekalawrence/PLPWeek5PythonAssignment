353f7ad878b637e221537493b3834f2ae1fcc15d
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-29 11:12:23 +0300

    Add files via upload

---
+++ Week5PythonAssignment.py
@@ -0,0 +1,17 @@
+#Designing a class
+class House:
+    """Base class representing a house"""
+    
+    def __init__(self, address, size_square_feet, num_bedrooms, num_bathrooms, color, price):
+        """A constructor to initialize the house class attributes"""
+        self.address = address
+        self.size_square_feet = size_square_feet
+        self.num_bedrooms = num_bedrooms
+        self.num_bashrooms = num_bathrooms
+        self.price = price
+        self.color = color
+        self.is_sold = False
+        
+    def calculate_price_per_sqft(self):
+        """Calculating the price per square foot"""
+        
