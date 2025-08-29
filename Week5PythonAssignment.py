0e63a89d91b110dde21773c82d09e79b47b9c1f6
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-08-29 11:23:46 +0300

    Add files via upload

--- Week5PythonAssignment.py
+++ Week5PythonAssignment.py
@@ -14,4 +14,18 @@ class House:
         
     def calculate_price_per_sqft(self):
         """Calculating the price per square foot"""
+        return self.price / self.size_square_feet
+    
+    def mark_as_sold(self):
+        """Mark the house as sold"""
+        self.is_sold = True
+        print(f"House at {self.address} has been sold!")
+        
+    def renovate(self, renovation_cost):
+        """Add renovation cost to the price"""
+        self.price += renovation_cost
+        print(f"Renovation completed. Redefined price is ${self.price:,.2f}")
+        
+    def get_house_info(self):
+        """Return full information"""
         
